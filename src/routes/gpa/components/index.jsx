47822fbc9623bef5e3da38096b25cbf8c4050516
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2016-12-31 01:14:46 -0800

    Adds input to GPA calculator

--- src/routes/gpa/components/index.jsx
+++ src/routes/gpa/components/index.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import InputBox from './inputBox';
 
 export default class GPA extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      gradePoints: 0,
+      credits: 0,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
   }
+
+  onInputChange(grade, units) {
+    const { gradePoints, credits } = this.state;
+    //const totalPoints = (this.gradeToNumber(grade) * units) + gradePoints;
+    const totalCredits = credits + units;
+    this.setState({
+      gradePoints: totalPoints,
+      units: totalCredits,
+    });
+  }
+
+
   render() {
     return (
       <div
@@ -12,7 +30,7 @@ export default class GPA extends React.Component {
         style={ { marginTop: '10%' } }
       >
         <div className='well infobox' style={ { margin: 'auto', width: '50%' } }>
-
+          <InputBox inputCount='1' />
         </div>
       </div>
     );
---
+++ src/routes/gpa/components/inputBox.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+export default class InputBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      grade: 0,
+      units: 0,
+    };
+  }
+
+  gradeToNumber(grade) {
+    switch (grade) {
+      case 'A':
+        return 4;
+      case 'A-':
+        return 3.7;
+      case 'B+':
+        return 3.3;
+      case 'B':
+        return 3.0;
+      case 'B-':
+        return 2.7;
+      case 'C+':
+        return 2.3;
+      case 'C':
+        return 2.0;
+      case 'C-':
+        return 1.7;
+      case 'D+':
+        return 1.3;
+      case 'D':
+        return 1.0;
+      default:
+        return 0;
+    }
+  }
+
+  render() {
+    const { inputCount } = this.props;
+    return (
+      <div className='row'>
+
+        <div className='form-group has-success label-floating is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='control-label'>Class</label>
+          <input type='text' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        </div>
+
+        <div className='form-group has-success label-floating is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='control-label'>Grade</label>
+          <input maxLength='1' type='text' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        </div>
+
+        <div className='form-group has-success label-floating is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='control-label'>Units</label>
+          <input type='number' maxLength='1' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+InputBox.propTypes = {
+  inputCount: React.PropTypes.string,
+};
