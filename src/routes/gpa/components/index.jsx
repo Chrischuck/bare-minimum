1262b88d417663f5d85cc22d63400c8085906b05
Chris Chuck <chrischuck35@gmail.com>
2017-01-07 10:39:55 -0800

    Finishes functionality of GPA calculator

--- src/routes/gpa/components/index.jsx
+++ src/routes/gpa/components/index.jsx
@@ -5,32 +5,129 @@ export default class GPA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gradePoints: 0,
-      credits: 0,
+      courses: {},
+      inputCount: 4,
+      gpa: '',
     };
 
-    this.onInputChange = this.onInputChange.bind(this);
+    this.stateFromChild = this.stateFromChild.bind(this);
+    this.addClass = this.addClass.bind(this);
+    this.calculate = this.calculate.bind(this);
+    this.showGpa = this.showGpa.bind(this);
   }
 
-  onInputChange(grade, units) {
-    const { gradePoints, credits } = this.state;
-    //const totalPoints = (this.gradeToNumber(grade) * units) + gradePoints;
-    const totalCredits = credits + units;
-    this.setState({
-      gradePoints: totalPoints,
-      units: totalCredits,
-    });
+  stateFromChild(id, course, grade, units) {
+    const { courses } = this.state;
+  //  const totalPoints = (grade * units) + gradePoints;
+  //  const totalCredits = credits + parseInt(units, 0);
+    courses[id] = { course, grade, units };
+    this.setState({ courses }, () => this.calculate());
   }
 
+  calculate() {
+    const { courses } = this.state;
+    const keys = Object.keys(courses);
+
+    let totalPoints = 0;
+    let totalCredits = 0;
+    for (let i = 0; i < keys.length; i += 1) {
+      const key = keys[i];
+      const { grade, units } = courses[key];
+
+      totalPoints += grade * parseInt(units, 0);
+      totalCredits += parseInt(units, 0);
+    }
+
+    const calculatedGpa = Math.ceil((totalPoints / totalCredits) * 100) / 100;
+    if ((calculatedGpa * 10) % 10 !== 0) {
+      const gpa = calculatedGpa.toFixed(2);
+      this.setState({ gpa });
+    } else {
+      const gpa = calculatedGpa.toFixed(1);
+      this.setState({ gpa });
+    }
+  }
+
+  addClass() {
+    const { inputCount } = this.state;
+    this.setState({ inputCount: inputCount + 1 });
+  }
+
+  showGpa() {
+    const { gpa } = this.state;
+    if (gpa) {
+      swal({
+        title: 'Nice!',
+        text: this.gpaStringBuilder(gpa),
+        confirmButtonColor: '#009688',
+        animation: 'slide-from-top',
+      });
+    } else {
+      swal({
+        title: 'Ugh oh!',
+        text: 'It appears you haven\'t added any classes!',
+        confirmButtonColor: '#009688',
+        animation: 'slide-from-top',
+        type: 'warning',
+      });
+    }
+  }
+
+  gpaStringBuilder(gpa) {
+    const numbericGpa = Number(gpa);
+    if (numbericGpa >= 4) {
+      return `Damn you're rocking a ${gpa}`;
+    }
+    if (numbericGpa < 4 && numbericGpa >= 3.8) {
+      return `Good job you have a solid ${gpa}`;
+    }
+    if (numbericGpa < 3.8 && numbericGpa >= 3) {
+      return `Awesome you got a ${gpa}`;
+    }
+    if (numbericGpa < 3 && numbericGpa >= 2.5) {
+      return `Not bad, you're getting a ${gpa}`;
+    }
+    if (numbericGpa < 2.5) {
+      return `You have a ${gpa}`;
+    }
+  }
 
   render() {
+    console.log(this.state)
+
+    const { inputCount } = this.state;
+    const inputs = [];
+    for (let i = 0; i < inputCount; i += 1) {
+      inputs.push(
+        <InputBox
+          inputCount={ i }
+          stateToParent={ this.stateFromChild }
+        />,
+      );
+    }
     return (
       <div
         className='container col-md-12'
         style={ { marginTop: '10%' } }
       >
         <div className='well infobox' style={ { margin: 'auto', width: '50%' } }>
-          <InputBox inputCount='1' />
+          <div className='col-md-offset-2'>
+            { inputs }
+          </div>
+
+          <div className='row'>
+            <div className='col-md-6' style={ { marginLeft: '8%', marginRight: '8%' } } >
+              <a className='btn btn-primary col-md-8' onClick={ this.addClass } >
+                Add Class <span className='glyphicon glyphicon-heart' />
+              </a>
+            </div>
+            <div className='col-md-6' style={ { marginLeft: '8%', marginRight: '8%' } } >
+              <a className='btn btn-primary col-md-8' onClick={ this.showGpa } >
+                Calculate <span className='glyphicon glyphicon-heart' />
+              </a>
+            </div>
+          </div>
+
         </div>
       </div>
     );
--- src/routes/gpa/components/inputBox.jsx
+++ src/routes/gpa/components/inputBox.jsx
@@ -6,14 +6,55 @@ export default class InputBox extends React.Component {
     super(props);
 
     this.state = {
-      grade: 0,
-      units: 0,
+      course: '',
+      grade: '',
+      units: '',
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.setInParent = this.setInParent.bind(this);
+  }
+
+  onInputChange(event, name) {
+    let val = '';
+
+    if (name === 'grade') {
+      val = this.gradeToNumber(event.target.value);
+    } else {
+      val = event.target.value;
+    }
+    this.setState({ [name]: val },
+      () => this.setInParent(),
+    );
+  }
+
+  setInParent() {
+    const { course, grade, units } = this.state;
+    if (grade && units) {
+      if (typeof grade !== 'number') {
+        this.setState({ grade: '' });
+        swal({
+          title: 'Ugh oh!',
+          text: `We dodn't recognize the grade of '${grade}'!`,
+          confirmButtonColor: '#009688',
+          animation: 'slide-from-top',
+          type: 'warning',
+        });
+      } else {
+        this.props.stateToParent(this.createId(), course, grade, units);
+      }
+    }
+  }
+
+  createId() {
+    const { inputCount } = this.props;
+    return `courseGpa${inputCount}`;
   }
 
   gradeToNumber(grade) {
-    switch (grade) {
+    const parsedGrade = grade.toUpperCase();
+    switch (parsedGrade) {
+      case 'A+':
+        return 4;
       case 'A':
         return 4;
       case 'A-':
@@ -34,33 +75,128 @@ export default class InputBox extends React.Component {
         return 1.3;
       case 'D':
         return 1.0;
-      default:
+      case 'D-':
         return 0;
+      case 'F':
+        return 0;
+      default:
+        return grade;
     }
   }
 
-  onInputChange(event, name) {
-    this.setState({ [name]: event.target.value });
+createRandGrades() {
+  const grade = Math.floor((Math.random() * 6) + 1);
+  switch (grade) {
+    case 1:
+      return 'A+';
+    case 2:
+      return 'A';
+    case 3:
+      return 'A-';
+    case 4:
+      return 'B+';
+    case 5:
+      return 'B';
+    default:
+      return 'A-';
   }
+}
+
+renderCourses(index) {
+  const courses = [
+    {
+      name: 'Math 133A',
+      grade: this.createRandGrades(),
+      units: '3',
+    },
+    {
+      name: 'Astro 10',
+      grade: this.createRandGrades(),
+      units: '2',
+    },
+    {
+      name: 'Phys 50',
+      grade: this.createRandGrades(),
+      units: '4',
+    },
+    {
+      name: 'Math 129A',
+      grade: this.createRandGrades(),
+      units: '3',
+    },
+    {
+      name: 'Math 42',
+      grade: this.createRandGrades(),
+      units: '3',
+    },
+    {
+      name: 'CS 46B',
+      grade: this.createRandGrades(),
+      units: '4',
+    },
+    {
+      name: 'Engl 18',
+      grade: this.createRandGrades(),
+      units: '2',
+    },
+    {
+      name: 'Kin 08',
+      grade: this.createRandGrades(),
+      units: '2',
+    },
+  ];
+  if (index < courses.length) {
+    return courses[index];
+  }
+  return {
+    name: 'ClassMcClassFace',
+    grade: 'A++',
+    units: '4.20',
+  };
+}
 
   render() {
     const { inputCount } = this.props;
+    const course = this.renderCourses(inputCount);
     return (
       <div className='row'>
 
-        <div className='form-group has-success label-floating is-empty col-md-3'>
-          <label htmlFor={ `${inputCount}grade` } className='control-label'>Class</label>
-          <input type='text' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        <div className='form-group has-success is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='form-label'>Class</label>
+          <input
+            type='text'
+            className='form-control'
+            id={ `${inputCount}grade` }
+            autoComplete='off'
+            onChange={ event => this.onInputChange(event, 'course') }
+            placeholder={ course.name }
+          />
         </div>
 
-        <div className='form-group has-success label-floating is-empty col-md-3'>
-          <label htmlFor={ `${inputCount}grade` } className='control-label'>Grade</label>
-          <input maxLength='1' type='text' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        <div className='form-group has-success is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='form-label'>Grade</label>
+          <input
+            maxLength='2'
+            type='text'
+            className='form-control'
+            id={ `${inputCount}grade` }
+            autoComplete='off'
+            onChange={ event => this.onInputChange(event, 'grade') }
+            placeholder={ course.grade }
+          />
         </div>
 
-        <div className='form-group has-success label-floating is-empty col-md-3'>
-          <label htmlFor={ `${inputCount}grade` } className='control-label'>Units</label>
-          <input type='number' maxLength='1' className='form-control' id={ `${inputCount}grade` } autoComplete='off' />
+        <div className='form-group has-success is-empty col-md-3'>
+          <label htmlFor={ `${inputCount}grade` } className='form-label'>Units</label>
+          <input
+            type='number'
+            maxLength='1'
+            className='form-control'
+            id={ `${inputCount}grade` }
+            autoComplete='off'
+            onChange={ event => this.onInputChange(event, 'units') }
+            placeholder={ course.units }
+          />
         </div>
 
       </div>
@@ -69,5 +205,6 @@ export default class InputBox extends React.Component {
 }
 
 InputBox.propTypes = {
-  inputCount: React.PropTypes.string,
+  inputCount: React.PropTypes.number,
+  stateToParent: React.PropTypes.func
 };
