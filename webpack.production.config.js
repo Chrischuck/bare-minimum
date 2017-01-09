9ab822ff62d49b70502549647cdf12b79ff65e90
Chris Chuck <chrischuck35@gmail.com>
2017-01-09 12:25:11 -0800

    Updates webpack production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,16 +1,21 @@
 /* eslint-disable */
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var Dashboard = require('webpack-dashboard');
 var DashboardPlugin = require('webpack-dashboard/plugin');
+
 var dashboard = new Dashboard();
 
 module.exports = {
 
   /////////// Sets up entra and output code ////////
+  devtool: 'cheap-module-source-map',
   context: __dirname + '/src', // directory webpack looks //
-  entry: './index.js', // file webpack looks at //
-
+  entry: [
+    'babel-polyfill',
+    './index.js', // file webpack looks at //
+  ],
   output: {
     filename: 'app.js', // output file name //
     path: __dirname + '/dist' // output file directory //
@@ -19,20 +24,37 @@ module.exports = {
 
   /////////// Sets up loaders ////////
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
+    extensions: ['', '.js', '.jsx', '.json', '.css']
   },
   module: {
     loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ["babel-loader"] },
+      { test: /\.eot/, loader: 'url-loader?mimetype=application/vnd.ms-fontobject' },
+      { test: /\.ttf/, loader: 'url-loader?mimetype=application/x-font-ttf' },
+      { test: /\.woff/, loader: 'url-loader?mimetype=application/font-woff' },
+      { test: /\.woff2/, loader: 'url-loader?mimetype=application/font-woff2' },
+      { test: /\.svg$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      { test: /\.png$/, loader: "url-loader?limit=100000" },
+      { test: /\.jpg$/, loader: "file" },
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"]
-      }
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: {
+          presets: ["es2015", "react", "stage-0"],
+          plugins: [
+              ["transform-decorators-legacy"],
+              ["transform-runtime"]
+            ]
+          }
+      },
+
+
     ]
   },
 
   devServer: {
-    publicPath: settings.output.publicPath,
     hot: true,
     quiet: true, // lets WebpackDashboard do its thing
     historyApiFallback: true,
@@ -46,10 +68,17 @@ module.exports = {
       filename: 'index.html',
       template: './index.html',
       inject: true
-    })
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+    new ExtractTextPlugin("styles.css"),
+    new webpack.DefinePlugin({
+  'process.env': {
+    'NODE_ENV': JSON.stringify('production')
+  }
+}),
   ]
-
-
-
 
 }
