16812fcb2fc0acc3d700d4d38ce2ccac7a5b175f
Chris <chrischuck35@gmail.com>
2018-01-12 14:02:48 -0800

    Edits staging config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -80,7 +80,7 @@ module.exports = {
       }
     }),
     new FaviconsWebpackPlugin({
-      logo: './assets/Bm144.png',
+      logo: './assets/logo.png',
       icons: {
        android: false,
        appleIcon: false,
