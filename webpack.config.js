3e40ae8c04a98d17cc5d98ace152df7219012db6
Chris Chuck <chrischuck@Chriss-MacBook-Pro.local>
2019-12-19 13:21:37 -0800

    consolidates webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var Dashboard = require('webpack-dashboard');
-var DashboardPlugin = require('webpack-dashboard/plugin');
-var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
-
-
-var dashboard = new Dashboard();
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
 
 module.exports = {
-
-  context: __dirname + '/src',
-  entry: [
-    './index.js', 
-  ],
-  output: {
-    filename: 'app.js',
-    path: __dirname + '/dist' 
+  context: __dirname + '/src', 
+  entry: {
+    app: './index.js', 
+    vendor: [
+      'preact',
+      'preact-helmet'
+    ]
   },
+  output: {
+    path: __dirname + '/dist',
+    filename: "[name].chunkhash.bundle.js",
+    chunkFilename: "[name].chunkhash.bundle.js",
+  },
+
   module: {
     rules: [
-      { test: /\.bundle\.js$/, use: [ { loader: 'bundle-loader', options: { lazy: true } } ] },
+      { test: /\.bundle\.js$/, loader: 'bundle-loader', options: { lazy: true } },
       { test: /\.eot/, loader: 'url-loader?mimetype=application/vnd.ms-fontobject' },
       { test: /\.ttf/, loader: 'url-loader?mimetype=application/x-font-ttf' },
       { test: /\.woff/, loader: 'url-loader?mimetype=application/font-woff' },
@@ -43,13 +45,71 @@ module.exports = {
   },
 
   plugins: [
-    //new DashboardPlugin(dashboard.setData),
     new HtmlWebpackPlugin({
       title: 'Bare Minimum',
       filename: 'index.html',
       template: './index.html',
-      inject: true
+      inject: true,
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+        minifyURLs: true,
+        minifyJS: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
     }),
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin("styles.css"),
+
+    ...(process.env.NODE_ENV === 'production' ? [
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+     new webpack.optimize.ModuleConcatenationPlugin(),
+     new webpack.optimize.AggressiveMergingPlugin(),
+     new FaviconsWebpackPlugin({
+       logo: './assets/logo.png',
+       icons: {
+        android: false,
+        appleIcon: false,
+        appleStartup: false,
+        coast: false,
+        favicons: true,
+        firefox: false,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false
+      }
+    }),
+     new CopyWebpackPlugin([
+       { from: '../_redirects' },
+       { from: '../manifest.json' }
+     ]),
+     new OfflinePlugin({
+       caches: {
+         main: ['*.bundle.js', 'index.html', 'styles.css', '*.png', '*.ico', 'manifest.json',' *.js'],
+         additional: [],
+         optional: []
+       },
+       cacheMaps: [
+         {
+           match: function(requestUrl) {
+             return new URL('/', location);
+           },
+           requestTypes: ['navigate']
+         }
+       ],
+       AppCache: false,
+       externals: ['/', '/grade-calculator'],
+       ServiceWorker: {
+         output: 'baremin-sw.js',
+         minify: true
+       }
+     })
+    ]: [])
   ]
 }
+
